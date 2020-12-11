261dbd4a199893ee79394c9a5b87f95bc7ee1e71
Michael Bordner <mbordner@gmail.com>
2020-12-11 01:00:15 -0500

    day 11

--- day11/p1/main.go
+++ day11/p1/main.go
@@ -1,5 +1,118 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2020/common/file"
+)
 
+func main() {
+	seats := getSeats()
+
+	var changes int
+	for {
+		seats, changes = getNextState(seats)
+		if changes == 0 {
+			break
+		}
+	}
+
+	occupied := 0
+	for r, row := range seats {
+		for c := range row {
+			if seats[r][c] == '#' {
+				occupied++
+			}
+		}
+	}
+
+	fmt.Println(occupied)
+}
+
+func getNextState(seats [][]byte) (next [][]byte, changes int) {
+	next = make([][]byte, len(seats), len(seats))
+	for i := range seats {
+		next[i] = make([]byte, len(seats[i]), len(seats[i]))
+	}
+
+	for r, row := range seats {
+		for c := range row {
+			n := seats[r][c]
+			if seats[r][c] != '.' { // floors never change
+				a := getAdjacent(seats, r, c)
+				switch seats[r][c] {
+				case 'L':
+					if a == 0 {
+						n = '#'
+					}
+				case '#':
+					if a > 3 {
+						n = 'L'
+					}
+				}
+			}
+			if n != seats[r][c] {
+				changes++
+			}
+			next[r][c] = n
+		}
+	}
+
+	return
+}
+
+func getAdjacent(seats [][]byte, row int, col int) int {
+	count := 0
+	if row > 0 {
+		if col > 0 {
+			if seats[row-1][col-1] == '#' { // upper left
+				count++
+			}
+		}
+		if seats[row-1][col] == '#' { // above
+			count++
+		}
+		if col < len(seats[0])-1 {
+			if seats[row-1][col+1] == '#' { // upper right
+				count++
+			}
+		}
+	}
+
+	if col > 0 {
+		if seats[row][col-1] == '#' { // left
+			count++
+		}
+	}
+	if col < len(seats[0])-1 {
+		if seats[row][col+1] == '#' { // right
+			count++
+		}
+	}
+	if row < len(seats)-1 {
+		if col > 0 {
+			if seats[row+1][col-1] == '#' { // lower left
+				count++
+			}
+		}
+		if seats[row+1][col] == '#' { // below
+			count++
+		}
+		if col < len(seats[0])-1 {
+			if seats[row+1][col+1] == '#' { // lower right
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func getSeats() [][]byte {
+	lines, _ := file.GetLines("../seats.txt")
+
+	seats := make([][]byte, len(lines), len(lines))
+	for i := range lines {
+		seats[i] = []byte(lines[i])
+	}
+
+	return seats
 }
--- day11/p2/main.go
+++ day11/p2/main.go
@@ -1,5 +1,157 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2020/common/file"
+)
 
+func main() {
+	seats := getSeats()
+
+	var changes int
+	for {
+		seats, changes = getNextState(seats)
+		if changes == 0 {
+			break
+		}
+	}
+
+	occupied := 0
+	for r, row := range seats {
+		for c := range row {
+			if seats[r][c] == '#' {
+				occupied++
+			}
+		}
+	}
+
+	fmt.Println(occupied)
+}
+
+func getNextState(seats [][]byte) (next [][]byte, changes int) {
+	next = make([][]byte, len(seats), len(seats))
+	for i := range seats {
+		next[i] = make([]byte, len(seats[i]), len(seats[i]))
+	}
+
+	for r, row := range seats {
+		for c := range row {
+			n := seats[r][c]
+			if seats[r][c] != '.' { // floors never change
+				a := getAdjacent(seats, r, c)
+				switch seats[r][c] {
+				case 'L':
+					if a == 0 {
+						n = '#'
+					}
+				case '#':
+					if a > 4 {
+						n = 'L'
+					}
+				}
+			}
+			if n != seats[r][c] {
+				changes++
+			}
+			next[r][c] = n
+		}
+	}
+
+	return
+}
+
+func getAdjacent(seats [][]byte, row int, col int) int {
+	count := 0
+	// left
+	for c := col - 1; c >= 0; c-- {
+		if seats[row][c] != '.' {
+			if seats[row][c] == '#' {
+				count++
+			}
+			break
+		}
+	}
+
+	// right
+	for c := col + 1; c <= len(seats[row])-1; c++ {
+		if seats[row][c] != '.' {
+			if seats[row][c] == '#' {
+				count++
+			}
+			break
+		}
+	}
+
+	// above
+	for r := row - 1; r >= 0; r-- {
+		if seats[r][col] != '.' {
+			if seats[r][col] == '#' {
+				count++
+			}
+			break
+		}
+	}
+
+	// below
+	for r := row + 1; r <= len(seats)-1; r++ {
+		if seats[r][col] != '.' {
+			if seats[r][col] == '#' {
+				count++
+			}
+			break
+		}
+	}
+
+	// upper left
+	for c, r := col-1, row-1; c >= 0 && r >= 0; c, r = c-1, r-1 {
+		if seats[r][c] != '.' {
+			if seats[r][c] == '#' {
+				count++
+			}
+			break
+		}
+	}
+
+	// lower left
+	for c, r := col-1, row+1; c >= 0 && r <= len(seats)-1; c, r = c-1, r+1 {
+		if seats[r][c] != '.' {
+			if seats[r][c] == '#' {
+				count++
+			}
+			break
+		}
+	}
+
+	// upper right
+	for c, r := col+1, row-1; c <= len(seats[row])-1 && r >= 0; c, r = c+1, r-1 {
+		if seats[r][c] != '.' {
+			if seats[r][c] == '#' {
+				count++
+			}
+			break
+		}
+	}
+
+	// lower right
+	for c, r := col+1, row+1; c <= len(seats[row])-1 && r <= len(seats)-1; c, r = c+1, r+1 {
+		if seats[r][c] != '.' {
+			if seats[r][c] == '#' {
+				count++
+			}
+			break
+		}
+	}
+
+	return count
+}
+
+func getSeats() [][]byte {
+	lines, _ := file.GetLines("../seats.txt")
+
+	seats := make([][]byte, len(lines), len(lines))
+	for i := range lines {
+		seats[i] = []byte(lines[i])
+	}
+
+	return seats
 }
