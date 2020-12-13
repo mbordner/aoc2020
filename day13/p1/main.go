a4ebfb0ca95ea935a8c2c3b536f4ca963fdcdd61
Michael Bordner <mbordner@gmail.com>
2020-12-13 18:46:44 -0500

    day 13

--- day13/p1/main.go
+++ day13/p1/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2020/common/file"
+	"strconv"
+	"strings"
+)
 
+type input struct {
+	arrival int64
+	buses   []int64
+}
+
+func main() {
+	data := getInputData()
+
+	var preferredID, departure int64
+
+	for i := range data.buses {
+		id := data.buses[i]
+		d := id
+		for j := int64(2); d < data.arrival; j++ {
+			d = j * id
+		}
+		if preferredID == 0 {
+			preferredID = id
+			departure = d
+
+		} else if d <= departure {
+			departure = d
+			preferredID = id
+		}
+	}
+
+	fmt.Println(preferredID, (departure-data.arrival)*preferredID)
+
+}
+
+func getInputData() input {
+	lines, _ := file.GetLines("../input.txt")
+
+	arrival, _ := strconv.ParseInt(lines[0], 10, 64)
+
+	tokens := strings.Split(lines[1], ",")
+
+	buses := make([]int64, 0, len(tokens))
+	for i := range tokens {
+		if tokens[i] != "x" {
+			tmp, _ := strconv.ParseInt(tokens[i], 10, 64)
+			buses = append(buses, tmp)
+		}
+	}
+
+	return input{arrival: arrival, buses: buses}
 }
--- day13/p2/main.go
+++ day13/p2/main.go
@@ -1,5 +1,87 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2020/common/file"
+	"sort"
+	"strconv"
+	"strings"
+)
 
+type input struct {
+	arrival uint64
+	buses   []uint64
+}
+
+type bus struct {
+	id     uint64
+	offset int64
+}
+
+func main() {
+	data := getInputData()
+
+	buses := make([]bus, 0, len(data.buses))
+
+	for i := range data.buses {
+		if data.buses[i] != uint64(0) {
+			b := bus{id: data.buses[i], offset: int64(i)}
+			buses = append(buses, b)
+		}
+	}
+
+	sort.SliceStable(buses, func(i, j int) bool {
+		return buses[i].id > buses[j].id
+	})
+
+	i := findRepeats(buses[0].id, buses[0].id, buses)
+
+	fmt.Println(i)
+}
+
+func findRepeats(i uint64, skip uint64, buses []bus) uint64 {
+	last := uint64(0)
+	for ; ; i += skip {
+		offset := buses[0].offset - buses[1].offset
+		if uint64(int64(i)-offset)%buses[1].id == 0 {
+
+			foundAll := true
+
+			for j := 1; j < len(buses)-1; j++ {
+				if uint64(int64(i)-buses[j].offset)%buses[j].id != 0 {
+					foundAll = false
+					break
+				}
+			}
+
+			if foundAll {
+				return uint64(int64(i) - buses[0].offset)
+			} else {
+				if last != uint64(0) {
+					return findRepeats(uint64(int64(i)-offset), i-last, buses[1:])
+				} else {
+					last = i
+				}
+			}
+		}
+
+	}
+}
+
+func getInputData() input {
+	lines, _ := file.GetLines("../input.txt")
+
+	arrival, _ := strconv.ParseUint(lines[0], 10, 64)
+
+	tokens := strings.Split(lines[1], ",")
+
+	buses := make([]uint64, len(tokens), len(tokens))
+	for i := range tokens {
+		if tokens[i] != "x" {
+			tmp, _ := strconv.ParseUint(tokens[i], 10, 64)
+			buses[i] = tmp
+		}
+	}
+
+	return input{arrival: arrival, buses: buses}
 }
