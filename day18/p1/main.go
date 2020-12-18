ae1687a738007c9f45dd4505fcd40a2b6cb335e6
Michael Bordner <mbordner@gmail.com>
2020-12-18 02:22:20 -0500

    day 18

--- day18/p1/main.go
+++ day18/p1/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2020/common/expression"
+	"github.com/mbordner/aoc2020/common/file"
+)
 
+func main() {
+	//p := expression.NewParser(`((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2`, expression.CompareOperator)
+	//fmt.Println(p.Eval())
+
+	var sum int64
+	lines, _ := file.GetLines("../input.txt")
+	for i := range lines {
+		p := expression.NewParser(lines[i], expression.CompareOperator)
+		sum += p.Eval()
+	}
+
+	fmt.Println(sum)
 }
--- day18/p2/main.go
+++ day18/p2/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2020/common/expression"
+	"github.com/mbordner/aoc2020/common/file"
+)
 
+var (
+	o = map[string]int{"+": 2, "*": 1}
+)
+
+func main() {
+	//t := expression.NewParser(`1 + 2 * 3 + 4 * 5 + 6`, func(op1, op2 string) int {
+	//	return o[op1] - o[op2]
+	//})
+	//fmt.Println(t.Eval())
+
+	var sum int64
+	lines, _ := file.GetLines("../input.txt")
+	for i := range lines {
+		p := expression.NewParser(lines[i], func(op1, op2 string) int {
+			return o[op1] - o[op2]
+		})
+		sum += p.Eval()
+	}
+
+	fmt.Println(sum)
 }
